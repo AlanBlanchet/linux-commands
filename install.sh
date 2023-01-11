867cffd2188ef6e48d0c168efad90469d8a7d1e1
AlanBlanchetNeovision <alan.blanchet@neovision.fr>
2023-01-11 11:21:02 +0100

    [DEV]Auto rebase if no conflict
    Shortcut to rebase branches if there is no conflicts

--- install.sh
+++ install.sh
@@ -13,4 +13,9 @@ sourceAliases="~/.aliases/source.sh"
 
 echo "source $sourceAliases" >> ~/.bashrc
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
+
 source "$sourceAliases"
--- src/git.sh
+++ src/git.sh
@@ -2,6 +2,24 @@ gr() {
     git rebase $@
 }
 
+grft() {
+    echo "Rebase on :"
+    f=$(git branch | cut -c 3- | gum choose)
+    for v in $(git branch | cut -c 3- | gum choose --no-limit)
+    do
+        gr $f $v
+        CONFLICTS=$(git ls-files -u | wc -l)
+        if [ "$CONFLICTS" -gt 0 ] ; then
+            echo "There is a merge conflict with $v"
+            git rebase --abort
+        else
+            if [ "$1" != "--no-push" ] || [ "$1" != "-n" ] ; then
+                gpf
+            fi
+        fi
+    done
+}
+
 grm() {
     if git show-ref --quiet refs/heads/master; then
         gr master
