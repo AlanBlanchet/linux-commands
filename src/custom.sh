bcf89e7000d06aa083e2e5b37ecb7061556ca0a5
Alan Blanchet <alan@alan-blanchet.fr>
2025-02-28 15:42:57 +0100

    [FIX]Added custom.sh

---
+++ src/custom.sh
@@ -0,0 +1,15 @@
+#
+# This file contains custom shell aliases and functions for personal use.
+# Add your frequently used commands, shortcuts, and utility functions here.
+#
+# This file is sourced from the main aliases configuration and
+# should be used for user-specific customizations that don't fit into other
+# categorized alias files.
+#
+# Usage:
+#   Add custom aliases like: alias mycommand='actual command'
+#   Add custom functions with:
+#   function myfunction() {
+#     # function code
+#   }
+#
